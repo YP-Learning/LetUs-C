da6026fe3dcb9aee7f42a100d6517aba9950e103
Yash Pawar <yashpawarp@gmail.com>
2021-09-27 18:44:22 +0530

    add question in solution file

--- src/Ch1-Getting-Started/F1.c
+++ src/Ch1-Getting-Started/F1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+// Ramesh's basic salary is input through the keyboard. 
+// His dearness allowance is 40% of basic salary, and 
+// house rent allowance is 20% of basic salary. 
+// Write a program to calculate his gross salary.
+
 int main(){
     int base_salary;
-    float gross_salary, dreamness_allowance = 0.4, house_rent_allowance = 0.2; // dreamness_allowance 40%, house_rent_allowance 20%
+    float gross_salary, dreamness_allowance = 0.4, house_rent_allowance = 0.2; 
+    // dreamness_allowance 40%, house_rent_allowance 20%
     
     printf("Please enter Base Salary:(in ₹ without `,`) ₹");
     scanf("%i",&base_salary);
